d9bfc5f01fa74588983b0be155bc8247ff11ce68
ficcanaso <fab.abb@gmail.com>
2023-07-05 17:13:41 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# ficcanaso
 # Input:
 # p, principal amount
 # t, time period in years
